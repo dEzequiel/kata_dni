b320f28aaaee306fbcec0ac1cf7a2655c7fc861c
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2021-12-29 23:57:19 +0100

    feat(get alphabet, get table letter, check valid letter): add methods to verify object inputs

--- src/create_dni.py
+++ src/create_dni.py
@@ -1,8 +1,12 @@
+from src.tabla_asignacion import TablaAsignacion
+
+
 class DNI:
     def __init__(self, chain=""):
         self.dni = chain
         self.healthy_number = False
         self.healthy_letter = False
+        self.table = TablaAsignacion()
 
     # methods to modify object instance
     def set_dni(self, chain):
@@ -25,10 +29,10 @@ class DNI:
 
     # checkers methods
     def check_dni(self):
-        if self.check_dni_length() and self.check_last_char_is_digit():
-            return True
-        else:
-            return False
+        self.set_healthy_number(
+            self.check_dni_length() and self.check_last_char_is_digit()
+        )
+        return self.get_healthy_number()
 
     def check_dni_length(self):
         if len(self.dni) == 9:
@@ -36,13 +40,39 @@ class DNI:
         else:
             return False
 
-    # ToDo longitud del dni input
-    # this method is for not passing a letter in middle of the string
     def check_last_char_is_digit(self):
         if self.dni[:-1].isdigit():
             return True
         else:
             return False
+
+    def check_letter(self):
+        if self.set_healthy_letter(
+            self.get_alphabet_dni().isupper()
+            and not self.get_alphabet_dni().isdigit()
+            and self.check_valid_letter()
+        ):
+            return self.get_healthy_letter()
+        else:
+            return False
+
+    def get_alphabet_dni(self):
+        return self.dni[-1]
+
+    def get_table_letter(self):
+        if self.get_healthy_number():
+            return self.table.calcular_letra(self.dni[:-1])
+        else:
+            return False
+
+    def check_valid_letter(self):
+        if self.get_healthy_number:
+            return self.get_alphabet_dni() == self.get_table_letter()
+        else:
+            return False
+
+    def check_cif(self):
+        pass
 
 
 if __name__ == "__main__":
