ff3cfb5f9f78fde91295ecd7f87c3b823bd3e2d5
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2021-12-29 18:59:00 +0100

    test: add some tests

--- test/test_dni.py
+++ test/test_dni.py
@@ -1,5 +1,5 @@
 import unittest
-from src.dni_cif import DNI
+from src.create_dni import DNI
 
 
 class DniClassTesting(unittest.TestCase):
@@ -7,11 +7,20 @@ class DniClassTesting(unittest.TestCase):
         testing_value = DNI("123456789")
         self.assertIsInstance(testing_value, DNI)
 
+    def test_check_dni(self):
+        value = DNI("123456789")
+        self.assertTrue(value.check_dni(), False)
+
+    def test_check_dni_length(self):
+        value = DNI("123456789")
+        self.assertTrue(value.check_dni_length(), True)
+
+    def test_check_last_char_is_int(self):
+        value = DNI("123456789")
+        self.assertTrue(value.check_last_char_is_int(), True)
+        value2 = DNI("123456789EE")
+        self.assertFalse(value2.check_last_char_is_int(), False)
+
 
 if __name__ == "__main__":
     unittest.main()
-
-
-# def check_dni():
-#     objeto = DNI()
-#     assert True == check_dni(objeto)
