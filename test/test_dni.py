0b578e6a3c707b1df7e5e0d8a65fc1d72d240f3e
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2021-12-29 19:07:12 +0100

    test: refactor asserts unit test

--- test/test_dni.py
+++ test/test_dni.py
@@ -8,18 +8,22 @@ class DniClassTesting(unittest.TestCase):
         self.assertIsInstance(testing_value, DNI)
 
     def test_check_dni(self):
-        value = DNI("123456789")
-        self.assertTrue(value.check_dni(), False)
+        value = DNI("")
+        self.assertTrue(value.check_dni())
 
     def test_check_dni_length(self):
         value = DNI("123456789")
-        self.assertTrue(value.check_dni_length(), True)
+        value2 = DNI("12345678")
+        self.assertTrue(value.check_dni_length())
+        self.assertFalse(value2.check_dni_length())
 
     def test_check_last_char_is_int(self):
         value = DNI("123456789")
-        self.assertTrue(value.check_last_char_is_int(), True)
+        self.assertTrue(value.check_last_char_is_int())
         value2 = DNI("123456789EE")
-        self.assertFalse(value2.check_last_char_is_int(), False)
+        self.assertFalse(
+            value2.check_last_char_is_int(),
+        )
 
 
 if __name__ == "__main__":
