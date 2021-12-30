28d11960305cf215e77b7dc57d1d995a392afa3f
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2021-12-30 01:13:24 +0100

    test: add test for get_alphabet_dni, get_numerical_dni and calculate_letter

--- test/test_dni.py
+++ test/test_dni.py
@@ -1,6 +1,8 @@
 import unittest
 from src.create_dni import DNI
 
+# from src.tabla_asignacion import TablaAsignacion
+
 
 class DniClassTesting(unittest.TestCase):
     def test_DNI_object_creation(self):
@@ -30,6 +32,22 @@ class DniClassTesting(unittest.TestCase):
         self.assertFalse(value2.check_last_char_is_digit())
         value3 = DNI("123,e6789")
         self.assertFalse(value3.check_last_char_is_digit())
+
+    def test_get_alphabet_dni(self):
+        value = DNI("95882054E")
+        self.assertEqual(value.get_alphabet_dni(), "E")
+
+    def test_get_numerical_dni(self):
+        value = DNI("42375166N")
+        self.assertEqual(value.get_numerical_dni(), "42375166")
+        value2 = DNI("12345678P")
+        self.assertNotEqual(value2.get_numerical_dni(), "12345678P")
+
+    def test_calculate_letter(self):
+        value = DNI("42375166N")
+        self.assertTrue(value.get_table_letter())
+        value2 = DNI("78484464N")
+        self.assertFalse(value2.get_table_letter())
 
 
 if __name__ == "__main__":
